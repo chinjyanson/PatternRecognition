bbdc7fd76007e0b9209ed445a4d14576d07291c9
chinjyanson <anson.chin22@imperial.ac.uk>
2026-01-12 13:33:58 +0000

    remove all asv files

---
+++ main.m
@@ -0,0 +1,15 @@
+base = "PR_CW_mat/";
+
+cylinder_normal = load(base + "cylinder_papillarray_single.mat");
+cylinder_rubber = load(base + "cylinder_rubber_papillarray_single.mat");
+cylinder_TPU    = load(base + "cylinder_TPU_papillarray_single.mat");
+hexagon_normal  = load(base + "hexagon_papillarray_single.mat");
+hexagon_rubber  = load(base + "hexagon_rubber_papillarray_single.mat");
+hexagon_TPU     = load(base + "hexagon_TPU_papillarray_single.mat");
+oblong_normal   = load(base + "oblong_papillarray_single.mat");
+oblong_rubber   = load(base + "oblong_rubber_papillarray_single.mat");
+oblong_TPU      = load(base + "oblong_TPU_papillarray_single.mat");
+
+disp("Data loading completed")
+
+plot(cylinder_normal.end_effector_poses)
---
+++ part1.m
@@ -0,0 +1,1 @@
+disp(cylinder_normal);
