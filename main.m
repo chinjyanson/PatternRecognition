79a2341f2785bf2fa69dda71c7ff78824b4a280e
chinjyanson <anson.chin22@imperial.ac.uk>
2026-01-12 14:40:16 +0000

    first round changes (part 1a, 1)

--- main.m
+++ main.m
@@ -1,5 +1,8 @@
+% Config
 base = "PR_CW_mat/";
+showFigs = true;
 
+% Data Loading
 cylinder_normal = load(base + "cylinder_papillarray_single.mat");
 cylinder_rubber = load(base + "cylinder_rubber_papillarray_single.mat");
 cylinder_TPU    = load(base + "cylinder_TPU_papillarray_single.mat");
@@ -9,7 +12,12 @@ hexagon_TPU     = load(base + "hexagon_TPU_papillarray_single.mat");
 oblong_normal   = load(base + "oblong_papillarray_single.mat");
 oblong_rubber   = load(base + "oblong_rubber_papillarray_single.mat");
 oblong_TPU      = load(base + "oblong_TPU_papillarray_single.mat");
-
 disp("Data loading completed")
 
-plot(cylinder_normal.end_effector_poses)
+% Part 1 
+part1(cylinder_normal, cylinder_rubber, cylinder_TPU, ...
+    hexagon_normal, hexagon_rubber, hexagon_TPU, showFigs)
+
+% Part 2
+
+
--- part1.m
+++ part1.m
@@ -1,1 +1,39 @@
-disp(cylinder_normal);
+function part1(varargin)
+    % Main function that calls others
+    arg_length = length(varargin);
+    showFigs = varargin{arg_length};
+    for i = 1:arg_length-1
+        displayEndEffectorPositions(varargin{i}, showFigs);
+    end
+end
+
+function displayEndEffectorPositions(data, showFigs)
+    if ~showFigs
+        return;  % Exit function early if not showing figures
+    end
+    pos = data.end_effector_poses;
+
+    figure
+    plot3(pos(:,1), pos(:,2), pos(:,3))
+    grid on
+    axis equal
+
+    xlabel('X')
+    ylabel('Y')
+    zlabel('Z')
+    title('End-Effector Position Trajectory')
+
+    figure
+    subplot(3,1,1)
+    plot(pos(:,4))
+    ylabel('Roll')
+
+    subplot(3,1,2)
+    plot(pos(:,5))
+    ylabel('Pitch')
+
+    subplot(3,1,3)
+    plot(pos(:,6))
+    ylabel('Yaw')
+    xlabel('Time (s)')
+end
